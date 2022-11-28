1fb2caf8a487592663a98432e163543445fab1f7
José Morano <j.morano@udc.es>
2022-11-28 15:50:36 +0100

    Improved normalize_path function.

--- lua/buffer_manager/utils.lua
+++ lua/buffer_manager/utils.lua
@@ -1,16 +1,14 @@
 local Path = require("plenary.path")
-local data_path = vim.fn.stdpath("data")
 
 local M = {}
 
-M.data_path = data_path
 
 function M.project_key()
   return vim.loop.cwd()
 end
 
 function M.normalize_path(item)
-  return Path:new(item):make_relative(M.project_key())
+  return Path:new(Path:new(item):absolute()):make_relative(M.project_key())
 end
 
 function M.is_white_space(str)
